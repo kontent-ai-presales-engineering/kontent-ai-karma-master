2b3ef84314843d67c9bb9a5281332ffdcc643eb6
Maarten van den Hooven <maartenv@kontent.ai>
2023-09-12 10:16:09 +0200

    added key

--- pages/products/[slug].tsx
+++ pages/products/[slug].tsx
@@ -108,7 +108,7 @@ const ProductDetail: FC<Props> = ({ product, siteCodename, defaultMetadata, home
           language={language}
         />
         {product.elements.keyFeatures.linkedItems.map(item =>
-          <li className="flex items-center space-x-3">
+          <li className="flex items-center space-x-3" key={item.system.id}>
             <svg className="flex-shrink-0 w-3.5 h-3.5 text-green-500 dark:text-green-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 12">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5.917 5.724 10.5 15 1.5" />
             </svg>
