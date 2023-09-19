430dff642a4a01397ecda6e880cc03967b26f13a
Maarten van den Hooven <maartenv@kontent.ai>
2023-09-19 12:34:34 +0200

    push release

--- pages/api/create-environment.ts
+++ pages/api/create-environment.ts
@@ -21,7 +21,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       res.status(400).end()
       return
     }
-
     const kms = new KontentManagementService()
     const vercel = new VercelService()
 
