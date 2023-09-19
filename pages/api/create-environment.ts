33c5303379f092f066ca0b792c64ee94717f6561
Maarten van den Hooven <maartenv@kontent.ai>
2023-09-19 12:42:04 +0200

    force role id

--- pages/api/create-environment.ts
+++ pages/api/create-environment.ts
@@ -26,7 +26,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     console.log("Get Role ID for inviting a user to the new environment with the environment role")
     const roleName = process.env.NEXT_PUBLIC_KONTENT_ENVIRONMENTROLE_CODENAME
-    const roleId = (await kms.getRole("environment-manager"))?.id
+    const roleId = "5b1074ba-54da-4f2d-acd5-45075ba756cb"
 
     console.log("Clone new enviroment")
     const newEnvironment = (await kms.cloneEnvironment(request.environment_name, [roleId]))
