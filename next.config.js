4fe36fa56b2541a5ae58cfe9b11e91b893d828ca
Maarten van den Hooven <maartenv@kontent.ai>
2023-10-02 14:51:29 +0200

    try maxDuration config

--- next.config.js
+++ next.config.js
@@ -15,18 +15,16 @@ module.exports = {
         {
             source: '/robots.txt',
             destination: '/api/robots'
-        }
+        },
+        {
+          source: '/vercel.json',
+          destination: '/public/data/vercel.json'
+      }
     ];
   },
   i18n,
   images: {
     loader: "custom",
     loaderFile: "./lib/imageLoader.ts",
-  },
-  "functions": {
-    "api/create-environment.ts": {
-      "memory": 3008,
-      "maxDuration": 300 // This function can run for a maximum of 300 seconds
-    }
   }
 }
