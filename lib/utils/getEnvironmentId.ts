fc0165cce0b3c20c699a531b7c3edd59b6b92630
Maarten van den Hooven <maartenv@kontent.ai>
2023-10-02 10:25:46 +0200

    get value from cookie

--- lib/utils/getEnvironmentId.ts
+++ lib/utils/getEnvironmentId.ts
@@ -3,6 +3,6 @@ import { NextResponse } from 'next/server'
 
 export default function getEnvironmentId() {
   const response = NextResponse.next()
-  const envid = response.cookies.get('kontent-ai-hermes-envid')
+  const envid = response.cookies.get('kontent-ai-hermes-envid').value
   return envid ? envid : process.env.NEXT_PUBLIC_KONTENT_ENVIRONMENT_ID
 }
