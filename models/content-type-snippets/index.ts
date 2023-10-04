a7002cf37cdc813bbb9e75093e0c3b57ec046d23
carlo-kontent <carlov@kontent.ai>
2023-10-04 11:50:57 +1100

    remove generated models fodler

--- models/content-type-snippets/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './openGraphMetadata';
-export * from './SEOMetadata';
--- models/taxonomies/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './articleCategory';
-export * from './channels';
-export * from './eventType';
-export * from './productCategory';
