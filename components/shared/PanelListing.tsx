58a641d8c253e123e1b50c18311a21fdb286fcc1
Jake Kula <71390735+jakekula@users.noreply.github.com>
2023-10-06 16:01:20 +1100

    Panel listing development - change to 3 panels

--- components/shared/PanelListing.tsx
+++ components/shared/PanelListing.tsx
@@ -12,9 +12,9 @@ export const PanelListingComponent: FC<Props> = props => {
 
   return (
     <section className="bg-gray-1 dark:bg-gray-000 py-10 px-12">
-      <div className="grid grid-flow-row gap-8 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <div className="grid grid-flow-row gap-8 text-neutral-600 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
         {props.item.elements.panels.linkedItems.map((link) => (
-          <div className="my-8 rounded shadow-lg shadow-gray-200 dark:shadow-gray-900 bg-white dark:bg-gray-800 duration-300 hover:-translate-y-1" key={link.system.id}>
+          <div className="my-8 rounded shadow-lg shadow-gray-200 dark:shadow-gray-900 bg-white dark:bg-gray-800 duration-300 hover:-translate-y-3" key={link.system.id}>
               <a href={link.elements.link.value} className="cursor-pointer">
                 <figure>                
                   <figcaption className="p-4">
