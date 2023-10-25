c24976e7c478aa7a87ade2060dea68b94c84a730
carlo-kontent <carlov@kontent.ai>
2023-10-25 13:04:18 +1100

    Product Listing and Product Item styling

--- components/listingPage/ProductItem.tsx
+++ components/listingPage/ProductItem.tsx
@@ -1,50 +1,67 @@
-import Image from "next/image"
-import Link from "next/link";
-import { FC } from "react";
+import Image from 'next/image';
+import Link from 'next/link';
+import { FC } from 'react';
 
-import { mainColorBgClass, mainColorTextClass } from "../../lib/constants/colors";
-import { useSiteCodename } from "../shared/siteCodenameContext";
-import { StandaloneSmartLinkButton } from "../shared/StandaloneSmartLinkButton";
+import {
+  mainColorBgClass,
+  mainColorTextClass,
+  mainColorHoverClass,
+} from '../../lib/constants/colors';
+import { useSiteCodename } from '../shared/siteCodenameContext';
+import { StandaloneSmartLinkButton } from '../shared/StandaloneSmartLinkButton';
 
 type Props = Readonly<{
   imageUrl: string;
   title: string;
   detailUrl: string;
   price: number | null;
-  category: string,
+  category: string;
   itemId?: string;
   itemName?: string;
 }>;
 
-export const ProductItem: FC<Props> = props => {
-  const siteCodename = useSiteCodename()
+export const ProductItem: FC<Props> = (props) => {
+  const siteCodename = useSiteCodename();
 
   return (
-    <li className="min-w-full m-0 p-0 relative rounded-lg shadow hover:shadow-xl transition-shadow border border-gray-200 cursor-pointer min-h-full">
+    <li className='min-w-full m-0 p-0 relative rounded-lg shadow hover:shadow-xl transition-shadow border border-gray-200 cursor-pointer min-h-full'>
       <Link
         href={props.detailUrl}
-        className="no-underline p-0 m-0"
+        className='no-underline p-0 m-0 justify-items-stretch'
       >
-      <StandaloneSmartLinkButton itemId={props.itemId} itemName={props.itemName} />
+        <StandaloneSmartLinkButton
+          itemId={props.itemId}
+          itemName={props.itemName}
+        />
 
-        <div className="flex flex-col gap-2">
-          <h5 className="px-4 pt-2 mt-2 text-center text-xl  tracking-wider font-semibold text-gray-900">{props.title}</h5>
-          <p className="m-0 text-center text-gray-500 text-base">{props.category}</p>
-          <figure className="w-full relative m-0 h-40">
+        <div className='flex flex-col gap-2'>
+          <h5 className='px-4 pt-2 mt-2 text-center text-xl  tracking-wider font-semibold text-gray-900'>
+            {props.title}
+          </h5>
+          <p className='m-0 text-center text-gray-500 text-base'>
+            {props.category}
+          </p>
+          <figure className='w-full relative m-0 h-40'>
             <Image
               src={props.imageUrl}
               alt={props.title}
               fill
-              sizes="(max-width: 635px) 100vw, (max-width: 1534px) 50vw, 25vw"
-              className="object-contain h-full w-full m-0 p-0 rounded-t-lg"
+              sizes='(max-width: 635px) 100vw, (max-width: 1534px) 50vw, 25vw'
+              className='object-contain h-full w-full m-0 p-0 rounded-t-lg'
             />
           </figure>
-          {props.price && <p className="m-0 text-center text-xl font-normal pb-2">{`${props.price}€`}</p>}
-          <button className={`${mainColorTextClass} block ml-auto w-fit mb-3 mr-4 font-semibold line-clamp-6 hover:bg-transparent border ${mainColorBgClass[siteCodename]} py-2 px-4 rounded`}>Detail</button>
+          {props.price && (
+            <p className='m-0 text-center text-xl font-normal pb-2'>{`${props.price}€`}</p>
+          )}
+          <button
+            className={`${mainColorBgClass[siteCodename]} ${mainColorTextClass[siteCodename]} ${mainColorHoverClass[siteCodename]} font-bold py-3 px-8 m-3 rounded duration-100 hover:scale-105 hover:drop-shadow`}
+          >
+            Detail
+          </button>
         </div>
       </Link>
     </li>
   );
-}
+};
 
-ProductItem.displayName = "ListItem";
+ProductItem.displayName = 'ListItem';
--- pages/[envId]/products/[slug].tsx
+++ pages/[envId]/products/[slug].tsx
@@ -1,19 +1,34 @@
-import { GetStaticPaths, GetStaticProps } from "next";
-import Image from "next/image";
+import { GetStaticPaths, GetStaticProps } from 'next';
+import Image from 'next/image';
 import { ParsedUrlQuery } from 'querystring';
-import { FC } from "react";
-import { AppPage } from "../../../components/shared/ui/appPage";
-import { getDefaultMetadata, getProductDetail, getProductItemsWithSlugs } from "../../../lib/services/kontentClient";
-import { ValidCollectionCodename } from "../../../lib/types/perCollection";
-import { defaultEnvId, siteCodename } from "../../../lib/utils/env";
-import { createElementSmartLink } from "../../../lib/utils/smartLinkUtils";
-import { WSL_WebSpotlightRoot, contentTypes, Product, SEOMetadata } from "../../../models"
-import { getHomepage } from "../../../lib/services/kontentClient";
-import { RichTextElement } from "../../../components/shared/richText/RichTextElement";
-import { mainColorBgClass, mainColorTextClass } from "../../../lib/constants/colors";
-import Link from "next/link";
-import { getEnvIdFromRouteParams, getPreviewApiKeyFromPreviewData } from "../../../lib/utils/pageUtils";
-
+import { FC } from 'react';
+import { AppPage } from '../../../components/shared/ui/appPage';
+import {
+  getDefaultMetadata,
+  getProductDetail,
+  getProductItemsWithSlugs,
+} from '../../../lib/services/kontentClient';
+import { ValidCollectionCodename } from '../../../lib/types/perCollection';
+import { defaultEnvId, siteCodename } from '../../../lib/utils/env';
+import { createElementSmartLink } from '../../../lib/utils/smartLinkUtils';
+import {
+  WSL_WebSpotlightRoot,
+  contentTypes,
+  Product,
+  SEOMetadata,
+} from '../../../models';
+import { getHomepage } from '../../../lib/services/kontentClient';
+import { RichTextElement } from '../../../components/shared/richText/RichTextElement';
+import {
+  mainColorBgClass,
+  mainColorTextClass,
+  mainColorHoverClass,
+} from '../../../lib/constants/colors';
+import Link from 'next/link';
+import {
+  getEnvIdFromRouteParams,
+  getPreviewApiKeyFromPreviewData,
+} from '../../../lib/utils/pageUtils';
 
 type Props = Readonly<{
   product: Product;
@@ -25,24 +40,25 @@ type Props = Readonly<{
 }>;
 
 interface IParams extends ParsedUrlQuery {
-  slug: string
+  slug: string;
 }
 
 export const getStaticPaths: GetStaticPaths = () =>
-  getProductItemsWithSlugs({ envId: defaultEnvId })
-    .then(products => ({
-      paths: products.map(product => ({
-        params: {
-          slug: product.elements.url.value,
-          envId: defaultEnvId
-        }
-      })),
-      fallback: 'blocking'
-    }));
+  getProductItemsWithSlugs({ envId: defaultEnvId }).then((products) => ({
+    paths: products.map((product) => ({
+      params: {
+        slug: product.elements.url.value,
+        envId: defaultEnvId,
+      },
+    })),
+    fallback: 'blocking',
+  }));
 
-export const getStaticProps: GetStaticProps<Props, IParams> = async (context) => {
+export const getStaticProps: GetStaticProps<Props, IParams> = async (
+  context
+) => {
   const slug = context.params?.slug;
-  const language = context.locale as string
+  const language = context.locale as string;
   const envId = getEnvIdFromRouteParams(context);
   const previewApiKey = getPreviewApiKeyFromPreviewData(context.previewData);
 
@@ -50,9 +66,22 @@ export const getStaticProps: GetStaticProps<Props, IParams> = async (context) =>
     return { notFound: true };
   }
 
-  const product = await getProductDetail({ envId, previewApiKey }, slug, !!context.preview, language);
-  const defaultMetadata = await getDefaultMetadata({ envId, previewApiKey }, !!context.preview, language);
-  const homepage = await getHomepage({ envId, previewApiKey }, !!context.preview, language);
+  const product = await getProductDetail(
+    { envId, previewApiKey },
+    slug,
+    !!context.preview,
+    language
+  );
+  const defaultMetadata = await getDefaultMetadata(
+    { envId, previewApiKey },
+    !!context.preview,
+    language
+  );
+  const homepage = await getHomepage(
+    { envId, previewApiKey },
+    !!context.preview,
+    language
+  );
 
   if (!product) {
     return { notFound: true };
@@ -65,44 +94,63 @@ export const getStaticProps: GetStaticProps<Props, IParams> = async (context) =>
       defaultMetadata,
       isPreview: !!context.preview,
       language: context.locale as string,
-      homepage: homepage
-    }
+      homepage: homepage,
+    },
   };
 };
 
 const widthLimit = 300;
 
-const ProductDetail: FC<Props> = ({ product, siteCodename, defaultMetadata, homepage, language, isPreview }) => (
+const ProductDetail: FC<Props> = ({
+  product,
+  siteCodename,
+  defaultMetadata,
+  homepage,
+  language,
+  isPreview,
+}) => (
   <AppPage
     item={product}
     siteCodename={siteCodename}
     homeContentItem={homepage}
     defaultMetadata={defaultMetadata}
-    pageType="Product"
+    pageType='Product'
     isPreview={isPreview}
   >
     <div>
       <h1
-        {...createElementSmartLink(contentTypes.product.elements.title.codename)}
+        {...createElementSmartLink(
+          contentTypes.product.elements.title.codename
+        )}
       >
         {product.elements.title.value}
       </h1>
-      <div {...createElementSmartLink(contentTypes.product.elements.product_image.codename)}>
-        {
-          product.elements.productImage.value[0] && (
-            <Image
-              src={product.elements.productImage.value[0].url}
-              alt={product.elements.productImage.value[0].description || product.elements.productImage.value[0].url.split('/').pop() || "Product image"}
-              width={widthLimit}
-              height={product.elements.productImage.value[0].height || 200}
-              className="object-cover"
-              priority
-            />
-          )
-        }
+      <div
+        {...createElementSmartLink(
+          contentTypes.product.elements.product_image.codename
+        )}
+      >
+        {product.elements.productImage.value[0] && (
+          <Image
+            src={product.elements.productImage.value[0].url}
+            alt={
+              product.elements.productImage.value[0].description ||
+              product.elements.productImage.value[0].url.split('/').pop() ||
+              'Product image'
+            }
+            width={widthLimit}
+            height={product.elements.productImage.value[0].height || 200}
+            className='object-cover'
+            priority
+          />
+        )}
       </div>
-      <div {...createElementSmartLink(contentTypes.product.elements.description.codename)}>
-        <h2 className="mb-3 text-gray-500 dark:text-gray-400">
+      <div
+        {...createElementSmartLink(
+          contentTypes.product.elements.description.codename
+        )}
+      >
+        <h2 className='mb-3 text-gray-500 dark:text-gray-400'>
           <RichTextElement
             element={product.elements.description}
             isInsideTable={false}
@@ -114,16 +162,23 @@ const ProductDetail: FC<Props> = ({ product, siteCodename, defaultMetadata, home
           isInsideTable={false}
           language={language}
         />
-        <Link href={`/html/productsheet.html?product=${product.system.codename}`} target="_blank">
-          <button
-            className={`${mainColorBgClass[siteCodename]} ${mainColorTextClass[siteCodename]} hover:bg-blue-700  font-bold py-2 px-4 m-3 rounded`}
+        {!product.system.codename ? (
+          <Link
+            href={`/html/productsheet.html?product=${product.system.codename}`}
+            target='_blank'
           >
-            Download datasheet
-          </button>
-        </Link>
+            <button
+              className={`${mainColorBgClass[siteCodename]} ${mainColorTextClass[siteCodename]} ${mainColorHoverClass[siteCodename]} font-bold py-3 px-8 m-3 rounded duration-100 hover:scale-105 hover:drop-shadow`}
+            >
+              Download datasheet
+            </button>
+          </Link>
+        ) : (
+          <p className='border-l-4 pl-4 mt-16 italic'>Datasheet coming soon</p>
+        )}
       </div>
     </div>
-  </AppPage >
+  </AppPage>
 );
 
 export default ProductDetail;
