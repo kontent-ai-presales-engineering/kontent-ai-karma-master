cc628efa1ac22efce3e3c96e627d4d009803bfca
Carlo Vinci - Kontent <carlov@kontent.ai>
2023-11-06 15:29:47 +1100

    Add margin below carosel

--- components/landingPage/ui/heroImage.tsx
+++ components/landingPage/ui/heroImage.tsx
@@ -13,7 +13,7 @@ type Props = Readonly<{
 
 export const HeroImage: FC<Props> = (props) => (
   <figure
-    className={`relative m-0 w-full h-[48rem] ${props.className ?? ''}`}
+    className={`relative m-0 w-full h-[48rem] mb-24 ${props.className ?? ''}`}
     {...createItemSmartLink(props.itemId, props.itemName)}
   >
     <Image
