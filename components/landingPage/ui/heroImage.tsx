f4dfc832792e15d7367276d7f3aa97a938d2c76b
Carlo Vinci - Kontent <carlov@kontent.ai>
2023-11-10 16:17:23 +1100

    revert banner colours

--- components/landingPage/ui/heroImage.tsx
+++ components/landingPage/ui/heroImage.tsx
@@ -37,7 +37,7 @@ export const HeroImage: FC<Props> = (props) => (
       />
     )}
     <div className='absolute inset-0 h-full flex flex-col items-center md:items-start justify-end pb-16 px-6 mx-10'>
-      <div className='backdrop-blur-sm bg-white/50 flex flex-col mx-auto rounded-lg py-4'>
+      <div className='backdrop-blur-sm bg-black/50 flex flex-col mx-auto rounded-lg py-4'>
         {props.children}
       </div>
     </div>
