5ae17d556877f49f47c5e4af96f7ea7c025080e6
Carlo Vinci - Kontent <carlov@kontent.ai>
2023-11-17 14:55:22 +1100

    Minor adjustments to the product listings

--- components/listingPage/ProductItem.tsx
+++ components/listingPage/ProductItem.tsx
@@ -27,20 +27,22 @@ export const ProductItem: FC<Props> = (props) => {
     <li className='min-w-full m-0 p-0 relative rounded-lg shadow hover:shadow-xl transition-shadow border border-gray-200 cursor-pointer min-h-full flex flex-col h-full'>
       <Link
         href={props.detailUrl}
-        className='no-underline p-0 m-0 justify-items-stretch flex flex-col min-h-full flex-1 h-full pb-5'
+        className='no-underline p-2 m-0 justify-items-stretch flex flex-col min-h-full flex-1 h-full'
       >
         <StandaloneSmartLinkButton
           itemId={props.itemId}
           itemName={props.itemName}
         />
 
-        <div className='flex flex-col gap-2'>
-          <h5 className='px-4 pt-2 mt-2 text-center text-lg font-semibold text-gray-900'>
-            {props.title}
-          </h5>
-          <p className='m-0 text-center text-gray-500 text-base'>
-            {props.category}
-          </p>
+        <div className='grid grid-cols-1 gap-3 h-full content-between'>
+          <div>
+            <h5 className='px-4 pt-2 mt-2 text-center text-lg font-semibold text-gray-900'>
+              {props.title}
+            </h5>
+            <p className='m-0 text-center text-gray-500 text-base'>
+              {props.category}
+            </p>
+          </div>
           <figure className='w-full relative m-0 h-40'>
             <Image
               src={props.imageUrl}
@@ -55,12 +57,11 @@ export const ProductItem: FC<Props> = (props) => {
               {`${props.price}€`}
             </p>
           )}
-
-          {/* <button
-            className={`${mainColorBgClass[siteCodename]} ${mainColorTextClass[siteCodename]} ${mainColorHoverClass[siteCodename]} font-bold py-3 px-8 m-3 rounded duration-100 hover:scale-105 hover:drop-shadow mt-auto`}
+          <button
+            className={`${mainColorBgClass[siteCodename]} ${mainColorTextClass[siteCodename]} ${mainColorHoverClass[siteCodename]} font-bold py-3 px-8 m-3 rounded duration-100 hover:scale-105 hover:drop-shadow mt-6 mx-auto`}
           >
             Detail
-          </button> */}
+          </button>
         </div>
       </Link>
     </li>
