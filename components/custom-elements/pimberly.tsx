be626f1e85a807f73613fe686a8dea076e559d7c
Maarten van den Hooven <maartenv@kontent.ai>
2024-03-22 13:12:47 +0100

    center images

--- components/custom-elements/pimberly.tsx
+++ components/custom-elements/pimberly.tsx
@@ -10,7 +10,7 @@ export const PimberlyCustomElement: React.FC<IProps> = ({
 }) => {
   return (
     <div className='custom-element'>
-      <div className='mb-3'>
+      <div className='mb-3 text-center'>
         {value?.split(',').map((imageUrl, i) => (
           <Image key={i} alt={value} height={300} src={imageUrl} />
         ))}
