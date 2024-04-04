fc0b7b55230c8b2501159cb6dd0275560f7f078c
Andrea N <andrea@narbot.com>
2024-04-04 18:51:00 -0400

    Update dependencies

---
+++ pages/api/og.tsx
@@ -0,0 +1,33 @@
+import { ImageResponse } from '@vercel/og';
+ 
+export const config = {
+  runtime: 'edge',
+};
+ 
+export default async function handler() {
+    console.log('Hello from the serverless function');
+    
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 40,
+          color: 'black',
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          padding: '50px 200px',
+          textAlign: 'center',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        👋 Hello 你好 नमस्ते こんにちは สวัสดีค่ะ 안녕 добрий день Hallá
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+    },
+  );
+}
