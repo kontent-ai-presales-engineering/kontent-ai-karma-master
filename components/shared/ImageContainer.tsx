a13530b5e1fa4bec9a0bd9e0be92f9c28dfa92a1
Maarten van den Hooven <maartenv@kontent.ai>
2024-06-03 14:15:00 +0200

    image container video option

--- components/shared/ImageContainer.tsx
+++ components/shared/ImageContainer.tsx
@@ -17,8 +17,7 @@ type Props = Readonly<{
 export const ImageContainerComponent: FC<Props> = (props) => {
   //Return null for personalized banners
   const personaId = getPersonaFromCookie();
-  if (props.personalized && props.item.elements.personas.value.length > 0 && !props.item.elements.personas.value.find(persona => persona.codename === personaId))
-  {
+  if (props.personalized && props.item.elements.personas.value.length > 0 && !props.item.elements.personas.value.find(persona => persona.codename === personaId)) {
     return null
   }
 
@@ -31,21 +30,28 @@ export const ImageContainerComponent: FC<Props> = (props) => {
     .withHeight(800)
     .getUrl()
 
-  const image = (
-      <Image
-        className='object-cover w-full md:w-3/4 lg:w-1/2 m-0 rounded-3xl mx-auto'
-        src={thumb}
-        width={thumbWidth}
-        height={thumbHeight}
-        priority
-        alt={thumbAlt as string}
-      />)
+  const mediaElement = props.item.elements.image.value[0];
+  const mediaComponent = mediaElement?.type?.startsWith('video') ? <video
+    src={mediaElement.url}
+    className='object-cover w-full md:w-3/4 lg:w-1/2 m-0 rounded-3xl mx-auto'
+    autoPlay={true}
+    loop={true}
+    muted={true}
+  /> : <Image
+    className='object-cover w-full md:w-3/4 lg:w-1/2 m-0 rounded-3xl mx-auto'
+    src={thumb}
+    width={thumbWidth}
+    height={thumbHeight}
+    priority
+    alt={thumbAlt as string}
+  />
+
   return (
     <div
       {...createItemSmartLink(props.item.system.id, props.item.system.name)}
       className='w-full rounded-lg flex my-12 lg:my-20 justify-center flex-col lg:flex-row'
     >
-      {props.item.elements.imageLocation.value[0].codename == 'left' && image}
+      {props.item.elements.imageLocation.value[0].codename == 'left' && mediaComponent}
       <div className='w-full lg:w-1/2 py-4 flex flex-col justify-center px-6 md:px-24'>
         <h2
           className='mt-0 font-semibold'
@@ -68,7 +74,7 @@ export const ImageContainerComponent: FC<Props> = (props) => {
           />
         </div>
       </div>
-      {props.item.elements.imageLocation.value[0].codename == 'right' && image}
+      {props.item.elements.imageLocation.value[0].codename == 'right' && mediaComponent}
     </div>
   );
 };
