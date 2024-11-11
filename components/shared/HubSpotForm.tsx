34b3d8bbb235a2594b0904b235ee226803600560
Maarten van den Hooven <maartenv@kontent.ai>
2024-11-11 10:19:01 +0100

    fix hubspot form

--- components/shared/HubSpotForm.tsx
+++ components/shared/HubSpotForm.tsx
@@ -16,11 +16,18 @@ export const HubSpotFormComponent: FC<Props> = (props) => {
       {...createItemSmartLink(props.item.system.id, props.item.system.name)}
       {...createElementSmartLink(contentTypes.form.elements.form.codename)}
       className={`bg-white shadow-md rounded px-8 pt-6 pb-8 my-16`}
-    >
+        >
       {props.item.elements.form.value && (
         <HubspotForm
-          portalId={props.item.elements.form.value.split('|')[1]}
-          formId={props.item.elements.form.value.split('|')[0]}
+          portalId={process.env.NEXT_PUBLIC_HUBSPOT_PORTAL_ID}
+          formId={(() => {
+            try {
+              return JSON.parse(props.item.elements.form.value).formId;
+            } catch (e) {
+              console.error('Invalid JSON in form value:', e);
+              return '';
+            }
+          })()}
           onSubmit={() => console.log('Submit!')}
           onReady={() => console.log('Form ready!')}
           loading={<div>Loading...</div>}
